2f3a066deb3ffb3aea25f5d56ef5eaa62f9bceea
LeoAvram <leo.avramovic@gmail.com>
2022-01-29 21:37:46 +0100

    add filtering category from home page

--- libs/products/src/lib/pages/product-list/product-list.component.ts
+++ libs/products/src/lib/pages/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { CategoriesService, Category, Product, ProductsService } from '@lav/products';
 import { Subject, takeUntil } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'products-list',
@@ -11,13 +12,18 @@ import { Subject, takeUntil } from 'rxjs';
 export class ProductListComponent implements OnInit, OnDestroy {
     products: Product[] = [];
     categories: Category[] = [];
+    isCategoryPage: boolean | undefined;
 
     endSubs$: Subject<any> = new Subject();
 
-    constructor(private productService: ProductsService, private categoryService: CategoriesService) {}
+    constructor(private productService: ProductsService, private categoryService: CategoriesService, private route: ActivatedRoute) {}
 
     ngOnInit(): void {
-        this.getProducts();
+        this.route.params.subscribe((params) => {
+            params['categoryid'] ? this.getProducts([params['categoryid']]) : this.getProducts();
+            params['categoryid'] ? (this.isCategoryPage = true) : (this.isCategoryPage = false);
+        });
+
         this.getCategories();
     }
 
--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     {
         path: 'products',
         component: ProductListComponent
+    },
+    {
+        path: 'category/:categoryid',
+        component: ProductListComponent
     }
 ];
 
